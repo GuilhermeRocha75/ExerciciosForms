a5bcdefa339d139a2e2b808db8a9b43275d25f82
GuilhermeRocha75 <guilhermeecheverritodarocha@gmail.com>
2024-05-31 15:09:54 -0300

    exercicio1

---
+++ Formularios/Exercicio1/script.js
@@ -0,0 +1,5 @@
+document.getElementById("contact-form").addEventListener("submit", function(event){
+    event.preventDefault(); 
+
+    document.getElementById("success-message").style.display = "block";
+});
